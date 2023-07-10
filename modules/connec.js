745e57f7f2fef7ff9fdda646df665496c49f800c
Daha Qalbi <qalbichamp5@gmail.com>
2023-07-10 01:10:11 -0700

    test

---
+++ modules/connec.js
@@ -0,0 +1,7 @@
+// Create a MySQL connection
+export const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+});
